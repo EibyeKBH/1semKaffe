6340421edd33fe3517abb4738f0ae6f7bca234c8
Benjamin Eibye <benjamin.eibye@gmail.com>
2020-10-06 23:21:32 +0200

    Skabelon til klasser

---
+++ Classes/bruger.py
@@ -0,0 +1,30 @@
+class Bruger():
+    def __init__(self, id, navn) -> None:
+        self.id = id
+        self.navn = navn
+        # Lav funktion der henter information omkring ID i bruger.json 
+
+
+class Service(Bruger):
+    def __init__(self, id) -> None:
+        # Vi skal hente info om brugern
+        pass
+
+    def påfyldIngrediens():
+        pass
+
+class Studerende(Bruger):
+    def __init__(self) -> None:
+        pass
+
+    def vælgKaffe():
+        pass
+
+    def swipeKort():
+        pass
+
+    def tagKort():
+        pass
+
+    def opfyldSald():
+        pass
---
+++ Classes/maskine.py
@@ -0,0 +1,12 @@
+class Kaffemaskine():
+    def __init__(self) -> None:
+        pass
+
+    def tilkaldService(self):
+        pass
+
+    def brygKaffe(self):
+        pass
+
+    def hævPenge(self):
+        pass
